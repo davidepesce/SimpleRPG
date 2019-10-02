560210d6f718e4025f4d49f090d15f3c833cff09
Davide Pesce <davidepesce1980@gmail.com>
2019-10-02 18:45:58 +0200

    Tutorial 6

---
+++ Entities/Player/Player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+# Player movement speed
+export var speed = 75
+
+func _physics_process(delta):
+	# Get player input
+	var direction: Vector2
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# If input is digital, normalize it for diagonal movement
+	if abs(direction.x) == 1 and abs(direction.y) == 1:
+		direction = direction.normalized()
+	
+	# Apply movement
+	var movement = speed * direction * delta
+	move_and_collide(movement)
