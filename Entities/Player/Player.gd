4c679d596925da08f6e335d598a46d8285debb7b
Davide Pesce <davidepesce1980@gmail.com>
2019-10-19 16:29:58 +0200

    Update code to Tutorial 7

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 # Player movement speed
 export var speed = 75
 
+# Player dragging flag
+var drag_enabled = false
+
+
 func _physics_process(delta):
 	# Get player input
 	var direction: Vector2
@@ -15,4 +19,25 @@ func _physics_process(delta):
 	
 	# Apply movement
 	var movement = speed * direction * delta
+	
+	# If dragging is enabled, use mouse position to calculate movement
+	if drag_enabled:
+		var new_position = get_global_mouse_position()
+		movement = new_position - position;
+		if movement.length() > (speed * delta):
+			movement = speed * delta * movement.normalized()
+	
 	move_and_collide(movement)
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			drag_enabled = event.pressed
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			drag_enabled = false
+
