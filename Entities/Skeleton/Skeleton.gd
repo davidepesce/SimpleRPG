4128faa88460c2420db4eff3580f165b5d66e1e7
Davide Pesce <davidepesce1980@gmail.com>
2020-02-17 14:26:39 +0100

    Using call_deferred to safely add a new potion inside on_area_entered() signal

--- Entities/Skeleton/Skeleton.gd
+++ Entities/Skeleton/Skeleton.gd
@@ -155,7 +155,7 @@ func hit(damage):
 		if rng.randf() <= 0.8:
 			var potion = potion_scene.instance()
 			potion.type = rng.randi() % 2
-			get_tree().root.get_node("Root").add_child(potion)
+			get_tree().root.get_node("Root").call_deferred("add_child", potion)
 			potion.position = position
 		# Add XP to player
 		player.add_xp(25)
