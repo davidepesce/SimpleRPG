428b785a61fb45b552c7a2c2b97892100c13c10c
Davide Pesce <davidepesce1980@gmail.com>
2020-04-05 12:44:25 +0200

    Added joypad support in menu

--- GUI/MenuPopup.gd
+++ GUI/MenuPopup.gd
@@ -20,42 +20,39 @@ func change_menu_color():
 
 
 func _input(event):
-	if event is InputEventKey:
-		var just_pressed = event.pressed and not event.echo
-		
-		if not visible:
-			if event.scancode == KEY_ESCAPE and just_pressed:
-				# Pause game
-				already_paused = get_tree().paused
-				get_tree().paused = true
-				# Reset the popup
-				selected_menu = 0
-				change_menu_color()
-				# Show popup
-				player.set_process_input(false)
-				popup()
-		else:
-			if event.scancode == KEY_DOWN and just_pressed:
-				selected_menu = (selected_menu + 1) % 3;
-				change_menu_color()
-			elif event.scancode == KEY_UP and just_pressed:
-				if selected_menu > 0:
-					selected_menu = selected_menu - 1
-				else:
-					selected_menu = 2
-				change_menu_color()
-			elif event.scancode == KEY_SPACE and just_pressed:
-				match selected_menu:
-					0:
-						# Resume game
-						if not already_paused:
-							get_tree().paused = false
-						player.set_process_input(true)
-						hide()
-					1:
-						# Restart game
-						get_tree().change_scene("res://Scenes/Main.tscn")
+	if not visible:
+		if Input.is_action_just_pressed("menu"):
+			# Pause game
+			already_paused = get_tree().paused
+			get_tree().paused = true
+			# Reset the popup
+			selected_menu = 0
+			change_menu_color()
+			# Show popup
+			player.set_process_input(false)
+			popup()
+	else:
+		if Input.is_action_just_pressed("ui_down"):
+			selected_menu = (selected_menu + 1) % 3;
+			change_menu_color()
+		elif Input.is_action_just_pressed("ui_up"):
+			if selected_menu > 0:
+				selected_menu = selected_menu - 1
+			else:
+				selected_menu = 2
+			change_menu_color()
+		elif Input.is_action_just_pressed("attack"):
+			match selected_menu:
+				0:
+					# Resume game
+					if not already_paused:
 						get_tree().paused = false
-					2:
-						# Quit game
-						get_tree().quit()
+					player.set_process_input(true)
+					hide()
+				1:
+					# Restart game
+					get_tree().change_scene("res://Scenes/Main.tscn")
+					get_tree().paused = false
+				2:
+					# Quit game
+					get_tree().quit()
